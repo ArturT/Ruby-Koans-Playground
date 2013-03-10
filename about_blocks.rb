1da6637c2645bdb0704e2c6e8d91e29d1492fce6
ArturT <arturtrzop@gmail.com>
2013-03-11 00:23:46 +0100

    Tests 177/280. Done about blocks.

--- about_blocks.rb
+++ about_blocks.rb
@@ -87,10 +87,10 @@ class AboutBlocks < EdgeCase::Koan
   end
 
   def test_methods_can_take_an_explicit_block_argument
-    assert_equal __, method_with_explicit_block { |n| n * 2 }
+    assert_equal 20, method_with_explicit_block { |n| n * 2 }
 
     add_one = lambda { |n| n + 1 }
-    assert_equal __, method_with_explicit_block(&add_one)
+    assert_equal 11, method_with_explicit_block(&add_one)
   end
 
 end
